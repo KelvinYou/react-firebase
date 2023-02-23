f18369ccebaa04cdf42c2a4731112dcb1ed2b7dd
KelvinYou <ykekelvin0220@gmail.com>
2023-02-23 13:44:18 +0800

    some common crud

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,17 @@ import React, { useEffect, useState } from 'react';
 import Logout from "../components/auth/Logout";
 import Center from "../components/utils/Center";
 import { db } from "../config/firebase";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import {
   collection, 
+  query,
+  onSnapshot,
+  doc,
+  getDocs,
   addDoc,
+  updateDoc,
+  deleteDoc,
 } from 'firebase/firestore';
 
 interface Props {}
@@ -14,8 +22,10 @@ const Home = ({}: Props) => {
 
   const [subject, setSubject] = useState("");
 
+  const [todos, setTodos] = useState<{ id: string }[]>([]);
+  
   const addSubmit = async () => {
-    console.info("add subject" + subject);
+    console.info("add: " + subject);
     if (subject !== "") {
       await addDoc(collection(db, "todos"), {
         subject,
@@ -24,6 +34,27 @@ const Home = ({}: Props) => {
       setSubject("");
     }
   }
+
+  const updateTodo = async (id: string, age: number) => {
+    const userDoc = doc(db, "todos", id);
+    const newFields = { age: age + 1 };
+    await updateDoc(userDoc, newFields);
+  };
+
+  const deleteTodo = async (id: string) => {
+    const userDoc = doc(db, "todos", id);
+    await deleteDoc(userDoc);
+  };
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(collection(db, "todos"));
+      setTodos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []);
+
 
   return (
     <Center>
@@ -37,6 +68,31 @@ const Home = ({}: Props) => {
         <div className="btn-container">
           <button onClick={ addSubmit }>Add-Todo</button>
         </div>
+        {todos.map((todo) => {
+        return (
+          <div>
+            {" "}
+            <h1>ID: {todo.id}</h1>
+            {/* <h1>Age: {todo.subject}</h1> */}
+            <button
+              onClick={() => {
+                // updatetodo(todo.id, todo.age);
+              }}
+            >
+              {" "}
+              Edit
+            </button>
+            <button
+              onClick={() => {
+                deleteTodo(todo.id);
+              }}
+            >
+              {" "}
+              Delete
+            </button>
+          </div>
+        );
+      })}
       <Logout />
     </Center>
   )
