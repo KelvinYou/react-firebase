c87cb87c118f4a6695d7f4ecd9a1dcb8be4acbb7
KelvinYou <ykekelvin0220@gmail.com>
2023-02-23 12:43:54 +0800

    update item to firestore

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -23,3 +24,4 @@ export const Firebase = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(Firebase);
 export const auth = getAuth();
 export const Providers = { google: new GoogleAuthProvider() };
+export const db = getFirestore(Firebase);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Logout from "../components/auth/Logout";
 import Center from "../components/utils/Center";
+import { db } from "../config/firebase";
+import {
+  collection, 
+  addDoc,
+} from 'firebase/firestore';
 
 interface Props {}
 
 const Home = ({}: Props) => {
   useEffect(() => {}, []); // eslint-disable-line react-hooks/exhaustive-deps
 
+  const [subject, setSubject] = useState("");
+
+  const addSubmit = async () => {
+    console.info("add subject" + subject);
+    if (subject !== "") {
+      await addDoc(collection(db, "todos"), {
+        subject,
+        completed: false,
+      });
+      setSubject("");
+    }
+  }
+
   return (
     <Center>
-        <Logout />
+        <div className="input-container">
+          <input type="text"
+            placeholder='what do you want to do?'
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          />
+        </div>
+        <div className="btn-container">
+          <button onClick={ addSubmit }>Add-Todo</button>
+        </div>
+      <Logout />
     </Center>
   )
 }
